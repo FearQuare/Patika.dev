e22831a55e287f61402175e1f35c9d98acbd28d9
Elmira <elmira.aras@protonmail.com>
2022-08-19 22:04:09 +0300

    19.08.2022 | Patika.dev python project

--- FlattenFunctionReverseFunction/main.py
+++ FlattenFunctionReverseFunction/main.py
@@ -9,3 +9,21 @@ a = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 flattenList = []
 flatten(a, flattenList)
 print(flattenList)
+
+def reverse(a, b):
+    lenght = len(a) - 1
+    while(lenght > -1):
+        if type(a[lenght]) is list:
+            b1 = []
+            reverse(a[lenght], b1)
+            b.append(b1)
+        else:
+            b.append(a[lenght])
+        lenght -= 1
+    return b
+reverseList = []
+b = [[1, 2], [3, 4], [5, 6, 7]]
+reverse(b, reverseList)
+print(reverseList)
+
+
